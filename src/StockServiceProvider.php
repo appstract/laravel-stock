dd1bdb72b1cb7896a040074a9a5b4fbb8ff2d28d
Peter <pkluver@gmail.com>
2020-04-11 12:17:25 +0200

    Update StockServiceProvider.php
    First publish config, then migrations. Otherwise config value is not present yet.

--- src/StockServiceProvider.php
+++ src/StockServiceProvider.php
@@ -11,14 +11,14 @@ class StockServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../database/migrations' => database_path('migrations'),
-            ], 'migrations');
-
+        if ($this->app->runningInConsole()) {            
             $this->publishes([
                 __DIR__ . '/../config' => config_path()
             ], 'config');
+            
+            $this->publishes([
+                __DIR__.'/../database/migrations' => database_path('migrations'),
+            ], 'migrations');            
         }
     }
 
